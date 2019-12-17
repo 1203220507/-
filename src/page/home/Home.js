29cfe865bc59340c0a095526f9346ddd4d513b30
MSTER_T <1203220507@QQ.COM>
2019-12-17 20:00:53 +0800

    部分首页

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,10 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { SearchBar, Flex, TabBar } from 'antd-mobile';
+import '../../assest/1.css';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                首页
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    backgroundColor: '#1296db',
+                    color: '#fff',
+                    padding: '0 0 0 10px'
+                }}>
+                    <span>房产全百科</span>
+                    <SearchBar placeholder="请输入关键词" style={{ flex: 1, backgroundColor: '#1296db', color: '#fff' }} />
+                </div>
+                <Flex>
+                    <Flex.Item style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '10px 0' }}>
+                        <img src={require('../../assest/dkw_daikuan-.png')} alt='我要贷款' width='30px'></img>
+                        <p style={{ margin: '5px 0 0 0', textAlign: 'center', fontSize: '12px', color: '#aaa' }}>我要贷款</p>
+                    </Flex.Item>
+                    <Flex.Item style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '10px 0' }}>
+                        <img src={require('../../assest/jisuanqi.png')} alt='我要贷款' width='30px'></img>
+                        <p style={{ margin: '5px 0 0 0', textAlign: 'center', fontSize: '12px', color: '#aaa' }}>我要贷款</p>
+                    </Flex.Item>
+                    <Flex.Item style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '10px 0' }}>
+                        <img src={require('../../assest/zhishi.png')} alt='我要贷款' width='30px'></img>
+                        <p style={{ margin: '5px 0 0 0', textAlign: 'center', fontSize: '12px', color: '#aaa' }}>我要贷款</p>
+                    </Flex.Item>
+                    <Flex.Item style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '10px 0' }}>
+                        <img src={require('../../assest/richscan_icon.png')} alt='我要贷款' width='30px'></img>
+                        <p style={{ margin: '5px 0 0 0', textAlign: 'center', fontSize: '12px', color: '#aaa' }}>我要贷款</p>
+                    </Flex.Item>
+                </Flex>
+
+                <TabBar
+                    unselectedTintColor="#949494"
+                    tintColor="#33A3F4"
+                    barTintColor="white"
+                >
+                    <TabBar.Item
+                        icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+                        title="首页"
+                        key="首页"
+                    >
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+                        title="微聊"
+                        key="微聊"
+                    >
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+                        title="推荐"
+                        key="推荐"
+                    >
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+                        title="我的"
+                        key="我的"
+                    >
+                    </TabBar.Item>
+                </TabBar>
             </div>
         )
     }
--- src/page/register/Register.js
+++ src/page/register/Register.js
@@ -1,11 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { List, InputItem, WhiteSpace, Checkbox, Button } from 'antd-mobile';
+
+const AgreeItem = Checkbox.AgreeItem;
 
 export default class Register extends Component {
+    send = () => {
+        console.log(1);
+    }
     render() {
         return (
-            <div>
-                注册
+            <div style={{ padding: '40px 20px' }}>
+                <List>
+                    <InputItem
+                        placeholder="请输入手机"
+                        type='text'
+                    >
+                    </InputItem>
+                </List>
+                <WhiteSpace size="xl" />
+
+                <List>
+                    <InputItem
+                        placeholder="请输入密码"
+                        type='password'
+                    >
+                    </InputItem>
+                </List>
+                <WhiteSpace size="xl" />
+
+                <List>
+                    <InputItem
+                        placeholder="请确认密码"
+                        type='password'
+                    >
+                    </InputItem>
+                </List>
+                <WhiteSpace size="xl" />
+
+                <List>
+                    <InputItem
+                        placeholder="请输入验证码"
+                        type='text'
+                        extra="获取验证码"
+                    >
+                    </InputItem>
+                </List>
+
+                <AgreeItem data-seed="logId" onChange={e => console.log('checkbox', e)} style={{ margin: '20px 0 30px' }}>
+                    <p style={{ margin: '0' }}>
+                        我已同意
+                        <a href='#/' style={{ color: '#1296db', fontSize: '14px' }}>《用户服务协议》</a>
+                        及
+                        <a href='#/' style={{ color: '#1296db', fontSize: '14px' }}>《隐私权政策》</a>
+                    </p>
+                </AgreeItem>
+
+                <Button type="primary">注册</Button>
+
+                <p>
+                    <a href='#/login' style={{ color: '#1296db', fontSize: '14px' }}>已有帐号</a>
+                </p>
             </div>
         )
+    }
+    componentDidMount() {
+        document.getElementsByClassName('am-input-extra')[0].addEventListener('click', this.send);
     }
 }
